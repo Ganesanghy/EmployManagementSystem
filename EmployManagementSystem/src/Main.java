f41ac97c6fc7e689b4e91525fe16f57d8be61f2a
Ganesanghy <ganeshghy0@gmail.com>
2023-06-16 16:15:38 +0530

    add new statement

--- EmployManagementSystem/src/Main.java
+++ EmployManagementSystem/src/Main.java
@@ -3,12 +3,13 @@ class mainmenu{
     void menu(){
         System.out.println("Press 1: To add new employee details");
         System.out.println("Press 2: To see exciting records");
+        System.out.println("press 3: To update");
     }
 }
 public class Main {
     public static void main(String[] args) {
         System.out.println("Employ Management System");
-        mainmenu me = new mainmenu();
-        me.menu();
+        mainmenu obj = new mainmenu();
+        obj.menu();
     }
 }
